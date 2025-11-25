7c999a833a348ec48a511b08867666514b073def
lohithakshay2008-gif <lohithakshay2008@gmail.com>
2025-11-25 15:00:58 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,14 @@
+int main()
+{
+	int a,b,c,d;
+	printf("enter a&b&c&d values");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	else if(a>b&&a>c&&a>d)
+	printf("a is big");
+	else if(b>c&&b>d)
+	printf("b is big");
+	else if(c>d)
+	printf("c is big");
+	else
+	printf("d is big")
+}
